2191194b9c4a9fc7e51e26a9f5e6c138b66d10e3
JasiaErnest <jasiaernest.lol@gmail.com>
2025-02-21 09:00:25 -0800

    max and getBalance methods

--- ErnestMethods.java
+++ ErnestMethods.java
@@ -1,7 +1,20 @@
-int max (int a, int b) {
-    if (a > b) {
-        return a;
-    } else {
-        return b;
+public class ErnestMethods {
+    public int max (int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
     }
+    
+    public int getBalance(DatabaseNode node) {
+        if(node == null) {
+            return 0;
+        } else {
+            int balance = findHeight(node.left) - findHeight(node.right);
+            //If the balance is negative, the right subtree is heavier
+            //If the balance is positive, the left subtree is heavier
+            return balance;
+        }
+    }    
 }
