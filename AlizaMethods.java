bb5811aa9c59fa62c280f887947f7dfa7ec1a8f1
lizeyliz <alizaeaton@gmail.com>
2025-02-22 13:51:54 -0800

    added rotateLeft

--- AlizaMethods.java
+++ AlizaMethods.java
@@ -1,37 +1,15 @@
 public class AlizaMethods {
-    static int height = -1;
+    DatabaseMethods methods = new DatabaseMethods();
 
-    // Helper function to find the height
-    // of a given node in the binary tree
-    static int findHeightUtil(DatabaseNode root, int id){
-        // Base Case: tree is empty
-        if (root == null) {
-            return -1;
-        }//end if
-
-        // Store the maximum height of
-        // the left and right subtree
-        int leftHeight = findHeightUtil(root.left, id);
-
-        int rightHeight = findHeightUtil(root.right, id);
-
-        // Update height of the current node
-        int ans = Math.max(leftHeight, rightHeight) + 1;
-
-        // If current node is the required node
-        if (root.getID() == id)
-            height = ans;
-
-        return ans;
-    }//end findHeightUtil
-
-    // Function to find the height of
-    // a given node in a Binary Tree
-    static int findHeight(DatabaseNode root, int id){
-        // Stores height of the Tree
-        findHeightUtil(root, id);
-
-        // Return the height
-        return height;
-    }//end findHeight
+    //rotates a node to the left: takes in node to rotate
+    DatabaseNode rotateLeft(DatabaseNode y) { 
+        DatabaseNode x = y.right; //x is y's original right child
+        DatabaseNode z = x.left; //z is x's original left child (saving so isn't lost)
+        x.left = y; //x's new left child is y
+        y.right = z;//y's new right child is z (x's original left child)
+        //update heights of y and x (even though z moves the height stays same)
+        methods.updateHeight(y);
+        methods.updateHeight(x);
+        return x;
+    }//end rotateLeft
 }//end AlizaMethods
--- DatabaseMethods.java
+++ DatabaseMethods.java
@@ -70,6 +70,18 @@ public class DatabaseMethods {
         return (n == null) ? 0 : height(n.right) - height(n.left); //return zero if null, else return height of right subtree - height left
     }//end get balance
 
+    //rotates a node to the left: takes in node to rotate
+    DatabaseNode rotateLeft(DatabaseNode y) { 
+        DatabaseNode x = y.right; //x is y's original right child
+        DatabaseNode z = x.left; //z is x's original left child (saving so isn't lost)
+        x.left = y; //x's new left child is y
+        y.right = z;//y's new right child is z (x's original left child)
+        //update heights of y and x (even though z moves the height stays same)
+        updateHeight(y);
+        updateHeight(x);
+        return x;
+    }//end rotateLeft
+
     //iterative inorder traversal, returns array of nodes inorder
     public DatabaseNode[] inorderArray(DatabaseNode root) {
         //initialize array to store nodes (size of tree)
